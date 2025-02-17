9d328ee25c53c10c47b6d33b85d829459178b6fd
30Elena <lenayrakova@gmail.com>
2025-02-17 19:51:17 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
+def is_very_long(text_user):
+    return len(text_user) > 12
+    
+def has_digit(letter):
+    return any(symbol.isdigit() for symbol in letter)
+
+def has_lower_letters(letter):
+    return any(symbol.islower() for symbol in letter)
+
+def has_upper_letters(letter):
+    return any(symbol.isupper() for symbol in letter)
+
+def has_symbols(letter):
+    return any(not symbol.isdigit() and not symbol.isalpha() for symbol in letter)
+
 def main():
     
     text_user = input('Введите пароль: ')
     score = 0 
-    
-    def is_very_long(text_user):
-        return len(text_user) > 12
-    
-    def has_digit(letter):
-        return any(symbol.isdigit() for symbol in letter)
-
-    def has_lower_letters(letter):
-        return any(symbol.islower() for symbol in letter)
-
-    def has_upper_letters(letter):
-        return any(symbol.isupper() for symbol in letter)
-
-    def has_symbols(letter):
-        return any(not symbol.isdigit() and not symbol.isalpha() for symbol in letter)
 
     functions = [
         is_very_long, 
